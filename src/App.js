58bea908261af72a0f5bb0a3426a723aeca5ddf0
BogyeumKim <namastae112@gmail.com>
2020-11-05 19:00:40 +0900

    반복문
    for가아닌 map으로 반복문
    기존 데이터주입을 customers.id/ customers.name .. 가아니라
    .map으로 반복시킴 이때 맵에는 벨류랑 키가있어야하니까 유니크한 id 키값으로 지정

--- src/App.js
+++ src/App.js
@@ -1,14 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
+import { render } from '@testing-library/react';
 
-function App() {
-  return (
-    <div className="gray-background">
-      <img src={logo} lat="logo" className="App-logo"/>
-      <h2>Let's React Start</h2>
+import Custom from './module/Custom';
 
-    </div>
-  );
+
+const customers = [{
+  'id':'qhrua25',
+  'image':'https://placeimg.com/64/64/1',
+  'name':'김보겸',
+  'email':'zz@zz.com',
+  'job':'취준생'
+},
+{
+  'id':'test2',
+  'image':'https://placeimg.com/64/64/2',
+  'name':'홍길동',
+  'email':'aa@zaa.com',
+  'job':'백수'
+},
+{
+  'id':'arry3',
+  'image':'https://placeimg.com/64/64/3',
+  'name':'손흥민',
+  'email':'qhrua25@naver.com',
+  'job':'갓수'
+}
+]
+
+
+class App extends Component{
+  render(){
+    return(
+      <div>
+        {
+          customers.map(c => {
+            return (<Custom
+                      key={c.id}
+                      id={c.id}
+                      name={c.name}
+                      image={c.image}
+                      email={c.email}
+                      job={c.job}
+            />)
+          })
+        }
+      </div>
+      );
+  }
+
 }
 
 export default App;
